401229feaa82b60fb1f54dd4661eb75864a83eb0
Beardo <ghislainroy.04@gmail.com>
2022-12-13 14:02:56 -0400

    Added the initial components needed for the REST API

---
+++ PokemonCenterAPI/PokemonCenterAPI/Controllers/PokemonController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonCenterAPI.DTO;
+
+namespace PokemonCenterAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonController : ControllerBase
+    {
+
+        public PokemonController() { 
+        
+        }
+
+        [HttpGet("ById")]
+        public PokemonDTO GetById([FromQuery] int id)
+        {
+            return new PokemonDTO();
+        }
+
+    }
+}
