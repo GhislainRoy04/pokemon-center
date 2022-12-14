4e12576a2d9b37ba7706ab83f9e2977fefe15ee4
Beardo <ghislainroy.04@gmail.com>
2022-12-14 06:39:51 -0400

    Added the base controller with some basic logging

---
+++ PokemonCenterAPI/PokemonCenterAPI/Controllers/BaseController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+public abstract class BaseController<T>: ControllerBase where T: BaseController<T>{
+
+    private ILogger<T> _logger;
+
+    protected ILogger<T> Logger => _logger ?? (_logger = HttpContext.RequestServices.GetService<ILogger<T>>());
+
+}
--- PokemonCenterAPI/PokemonCenterAPI/Controllers/PokemonController.cs
+++ PokemonCenterAPI/PokemonCenterAPI/Controllers/PokemonController.cs
@@ -6,7 +6,7 @@ namespace PokemonCenterAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class PokemonController : ControllerBase
+    public class PokemonController : BaseController<PokemonController>
     {
 
         private IPokemonService pokemonService;
@@ -18,41 +18,49 @@ namespace PokemonCenterAPI.Controllers
         [HttpGet("ById")]
         public List<PokemonDTO> GetById([FromQuery] int id)
         {
+            Logger.LogInformation("Get by ID called.");
             return pokemonService.GetById(id);
         }
 
         [HttpGet("GetAllByTypes")]
         public List<PokemonDTO> GetAllByTypes([FromQuery] List<string> types){
+            Logger.LogInformation("Get all by types called.");
             return pokemonService.GetAllByTypes(types);
         }
 
         [HttpGet("GetAllPaginated")]
         public List<PokemonDTO> GetAllPaginated([FromQuery] int page, [FromQuery] int pageSize){
+            Logger.LogInformation("Get all paginated called.");
             return pokemonService.GetAllPaginated(page, pageSize);
         }
 
         [HttpGet("SearchByName")]
         public List<PokemonDTO> SearchByName(string name){
+            Logger.LogInformation("Search by name called.");
             return pokemonService.SearchByName(name);
         }
 
         [HttpGet("GetAll")]
         public List<PokemonDTO> GetAll(){
+            Logger.LogInformation("Get all called.");
             return pokemonService.GetAll();
         }
 
         [HttpPost]
         public int Create([FromBody] PokemonDTO pokemon){
+            Logger.LogInformation("Create called.");
             return pokemonService.Create(pokemon);
         }
 
         [HttpPut]
         public bool Update([FromBody] PokemonDTO pokemon){
+            Logger.LogInformation("Update called.");
             return pokemonService.Update(pokemon);
         }
 
         [HttpDelete]
         public void Delete([FromQuery] int id){
+            Logger.LogInformation("Delete called.");
             pokemonService.Delete(id);
         }
 
